0459d45991d1146ebdc54f3bd679d2d5154283ad
suresh-guttikonda <guttikondasuresh7@gmail.com>
2020-10-27 12:33:27 +0100

    updates testing py code

--- src/launch_igibson.py
+++ src/launch_igibson.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
-import os
-from gibson2.envs.locomotor_env import NavigateEnv
+from gibson2.envs.locomotor_env import NavigateRandomEnv
 from gibson2.utils.utils import parse_config
 from gibson2.utils.assets_utils import get_model_path
+from gibson2.core.render.profiler import Profiler
+import os
 from PIL import Image
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+from transforms3d.euler import quat2euler
 
 def load_floor(config_data: dict) -> np.ndarray:
     """
     """
 
-    print(type(config_data))
     model_id = config_data['model_id']
     erosion = config_data['trav_map_erosion']
     floors = 0
@@ -41,6 +42,45 @@ def load_floor(config_data: dict) -> np.ndarray:
 
     return trav_map
 
+def getKey() -> int:
+    key = input('Enter key: ')
+    if key == '':
+        key = -1
+    else:
+        key = ord(key)
+    return key
+
+def teleop_robot(nav_env: NavigateEnv):
+    """
+    """
+
+    msg = """
+Moving around:
+    w
+a       d
+    s
+anything else : stop
+q to quit
+"""
+    print(msg)
+    state = nav_env.reset()
+    turtlebot = nav_env.robots[0]
+    for i in range(200):
+        key = getKey()
+        if key == 113:
+            # quit
+            break
+        elif (key, ) in turtlebot.keys_to_action.keys():
+            action = turtlebot.keys_to_action[(key, )]
+        else:
+            action = turtlebot.keys_to_action[()]
+
+        state, reward, done, info = nav_env.step(action)
+        print(state['sensor'], turtlebot.get_position(), quat2euler(turtlebot.get_orientation()))
+        if done:
+            print('Episode finished')
+            break
+
 def main():
     curr_dir_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -49,11 +89,9 @@ def main():
     config_data = parse_config(config_file_path)
 
     trav_map = load_floor(config_data)
-    print(np.unique(trav_map))
 
-    nav_env = NavigateEnv(config_file=config_file_path, mode='gui')
-    state = nav_env.reset()
-    print(state['sensor'])
+    nav_env = NavigateRandomEnv(config_file=config_file_path, mode='gui')
+    teleop_robot(nav_env)
 
 if __name__ == '__main__':
     main()
