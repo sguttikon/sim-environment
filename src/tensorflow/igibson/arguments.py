bug fix in transform maps scaling

src/tensorflow/igibson/arguments.py
@@ -22,7 +22,7 @@ def parse_args():
 
     # input configuration
     argparser.add_argument('--map_pixel_in_meters', type=float, default=0.1, help='The width (and height) of a pixel of the map in meters. Defaults to 0.1 for iGibson environment.')
-
+    argparser.add_argument('--agent', type=str, default='random', help='Agent used to sample actions in environment. Possible values: random / pretrained / manual.')
     argparser.add_argument('--init_particles_distr', type=str, default='gaussian', help='Distribution of initial particles. Possible values: gaussian / uniform.')
     argparser.add_argument('--init_particles_std', nargs='*', default=["0.3", "0.523599"], help='Standard deviations for generated initial particles for tracking distribution. Values: translation std (meters), rotation std (radians)')
     argparser.add_argument('--trajlen', type=int, default=24, help='Length of trajectories.')
@@ -38,7 +38,10 @@ def parse_args():
     argparser.add_argument('--learningrate', type=float, default=0.0025, help='Initial learning rate for training.')
     argparser.add_argument('--epochs', type=int, default=1, help='Number of epochs for training.')
 
-    argparser.add_argument('--load', type=str, default='', help='Load a previously trained model from a checkpoint file.')
+    argparser.add_argument('--pfnet_load', type=str, default='', help='Load a previously trained pfnet model from a checkpoint file.')
+    argparser.add_argument('--action_load', type=str, default='./ppo_igibson', help='Load a pretrained action sampler model.')
+    argparser.add_argument('--mode', type=str, default='headless', help='Render iGibson environment. Possible values: headless / gui.')
+    argparser.add_argument('--max_step', type=int, default='100', help='Max step per iGibson environment episode.')
     argparser.add_argument('--seed', type=int, default='42', help='Fix the random seed of numpy and tensorflow.')
     argparser.add_argument('--logpath', type=str, default='./log/', help='Specify path for logs.')
     argparser.add_argument('--gpu_num', type=int, default='0', help='use gpu no. to train')
@@ -50,6 +53,11 @@ def parse_args():
     params.init_particles_std = np.array(params.init_particles_std, np.float32)
 
     assert params.init_particles_distr in ['gaussian', 'uniform']
+    assert params.agent in ['manual', 'pretrained', 'random']
+    assert params.mode in ['headless', 'gui']
+
+    # iGibson env config file
+    params.config_filename = os.path.join('./configs/', 'turtlebot_demo.yaml')
 
     # build initial covariance matrix of particles, in pixels and radians
     particle_std = params.init_particles_std.copy()
@@ -66,6 +74,7 @@ def parse_args():
 
     #HACK hardcode for floor map
     params.global_map_size = (100, 100, 1)
+    params.window_scaler = None
 
     # filter out info and warning messages
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'

src/tensorflow/igibson/iGibson_env.py
@@ -512,6 +512,7 @@ class iGibsonEnv(BaseEnv):
                 particles.append([pos[0], pos[1], orn[2]])  # [x, y, theta]
 
                 sample_i = sample_i + 1
+            particles = np.array(particles) # [num_particles, 3]
         else:
             # gaussian distribution
             assert list(robot_pose.shape) == [3]
@@ -522,7 +523,7 @@ class iGibsonEnv(BaseEnv):
             # sample particles from the Gaussian, centered around the offset
             particles.append(np.random.multivariate_normal(mean=center, cov=particles_cov, size=num_particles))
 
-        particles = np.array(particles).squeeze(axis=0) # [num_particles, 3]
+            particles = np.array(particles).squeeze(axis=0) # [num_particles, 3]
         return particles
 
     def filter_collision_links(self, collision_links):

src/tensorflow/igibson/train.py
@@ -45,13 +45,17 @@ def run_training(params):
     # create gym env
     env = iGibsonEnv(config_file=params.config_filename, mode=params.mode,
                 action_timestep=1 / 10.0, physics_timestep=1 / 240.0,
-                device_idx=params.gpu_num)
+                device_idx=params.gpu_num, max_step=params.max_step)
 
     # create pf model
-    model = pfnet.pfnet_model(params)
+    pfnet_model = pfnet.pfnet_model(params)
 
     # get pretrained action model
-    action_model = datautils.load_action_model(env, params.gpu_num, params.action_load)
+    if params.action_load:
+        print("=====> Loading action sampler from " + params.action_load)
+        action_model = datautils.load_action_model(env, params.gpu_num, params.action_load)
+    else:
+        action_model = None
 
     # Adam optimizer.
     optimizer = tf.optimizers.Adam(learning_rate=params.learningrate)
@@ -82,7 +86,7 @@ def run_training(params):
             # if stateful: reset RNN s.t. initial_state is set to initial particles and weights
             # if non-stateful: pass the state explicity every step
             if params.stateful:
-                model.layers[-1].reset_states(state)    # RNN layer
+                pfnet_model.layers[-1].reset_states(state)    # RNN layer
 
             # run training over trajectory
             input = [observation, odometry]
@@ -91,7 +95,7 @@ def run_training(params):
             # enable auto-differentiation
             with tf.GradientTape() as tape:
                 # forward pass
-                output, state = model(model_input, training=True)
+                output, state = pfnet_model(model_input, training=True)
 
                 # compute loss
                 particle_states, particle_weights = output
@@ -99,8 +103,8 @@ def run_training(params):
                 loss_pred = loss_dict['pred']
 
             # compute gradients of the trainable variables with respect to the loss
-            gradients = tape.gradient(loss_pred, model.trainable_weights)
-            gradients = list(zip(gradients, model.trainable_weights))
+            gradients = tape.gradient(loss_pred, pfnet_model.trainable_weights)
+            gradients = list(zip(gradients, pfnet_model.trainable_weights))
 
             # run one step of gradient descent
             optimizer.apply_gradients(gradients)
@@ -112,7 +116,7 @@ def run_training(params):
 
         # Save the weights
         print("=====> saving trained model ")
-        model.save_weights(params.train_log_dir + f'/chks/checkpoint_{epoch}_{train_loss.result():03.3f}/pfnet_checkpoint')
+        pfnet_model.save_weights(params.train_log_dir + f'/chks/checkpoint_{epoch}_{train_loss.result():03.3f}/pfnet_checkpoint')
 
         if params.run_validation:
             # run validation over all validation samples in an epoch
@@ -132,14 +136,14 @@ def run_training(params):
                 # if stateful: reset RNN s.t. initial_state is set to initial particles and weights
                 # if non-stateful: pass the state explicity every step
                 if params.stateful:
-                    model.layers[-1].reset_states(state)    # RNN layer
+                    pfnet_model.layers[-1].reset_states(state)    # RNN layer
 
                 # run validation over trajectory
                 input = [observation, odometry]
                 model_input = (input, state)
 
                 # forward pass
-                output, state = model(model_input, training=False)
+                output, state = pfnet_model(model_input, training=False)
 
                 # compute loss
                 particle_states, particle_weights = output
@@ -153,7 +157,7 @@ def run_training(params):
                 tf.summary.scalar('loss', test_loss.result(), step=epoch)
 
             # Save the weights
-            model.save_weights(params.test_log_dir + f'/chks/checkpoint_{epoch}_{test_loss.result():03.3f}/pfnet_checkpoint')
+            pfnet_model.save_weights(params.test_log_dir + f'/chks/checkpoint_{epoch}_{test_loss.result():03.3f}/pfnet_checkpoint')
 
         print(f'Epoch {epoch}, train loss: {train_loss.result():03.3f}, test loss: {test_loss.result():03.3f}')
 
@@ -175,11 +179,7 @@ if __name__ == '__main__':
     params.train_log_dir = 'logs/' + current_time + '/train/'
     params.test_log_dir = 'logs/' + current_time + '/test/'
 
-    params.mode = 'headless'
-    params.agent = 'pretrained'
     params.run_validation = True
-    params.action_load = './ppo_igibson'
-    params.config_filename = os.path.join('./configs/', 'turtlebot_demo.yaml')
 
     params.output = 'training_results.log'
 

src/tensorflow/pfnet/pfnet.py
@@ -105,7 +105,7 @@ class PFCell(keras.layers.AbstractRNNCell):
         batch_size, num_particles = particle_states.shape.as_list()[:2]
 
         # transform global maps to local maps
-        local_maps = self.transform_maps(global_map, particle_states, (28, 28))
+        local_maps = self.transform_maps(global_map, particle_states, (28, 28), self.params.window_scaler)
 
         # rescale from [0, 2] to [-1, 1]    -> optional
         local_maps = -(local_maps - 1)
@@ -225,12 +225,13 @@ class PFCell(keras.layers.AbstractRNNCell):
 
         return tf.stack([part_x + delta_x , part_y + delta_y, part_th + delta_th], axis=-1)   # (bs, k, 3)
 
-    def transform_maps(self, global_map, particle_states, local_map_size):
+    def transform_maps(self, global_map, particle_states, local_map_size, window_scaler=None):
         """
         Implements global to local map transformation
         :param global_map: global map input (batch, None, None, ch)
         :param particle_states: particle states that define local view for transformation (batch, k, 3)
         :param local_map_size: size of output local maps (height, width)
+        :param window_scaler: global map will be down-scaled by some int factor
         :return (batch, k, local_map_size[0], local_map_size[1], ch): each local map shows different transformation
             of global map corresponding to particle state
         """
@@ -248,8 +249,6 @@ class PFCell(keras.layers.AbstractRNNCell):
         width_inverse = 1.0 / global_width
         zero = tf.constant(0, dtype=tf.float32, shape=(total_samples, ))
         one = tf.constant(1, dtype=tf.float32, shape=(total_samples, ))
-
-        window_scaler = 8.0 # global map will be down-scaled by some factor
 
         # normalize orientations and precompute cos and sin functions
         theta = -flat_states[:, 2] - 0.5 * np.pi
@@ -270,8 +269,13 @@ class PFCell(keras.layers.AbstractRNNCell):
         rotm = tf.reshape(rotm, [total_samples, 3, 3])
 
         # 3: scale down the map
-        scale_x = tf.fill((total_samples, ), float(local_map_size[1] * window_scaler) * width_inverse)
-        scale_y = tf.fill((total_samples, ), float(local_map_size[0] * window_scaler) * height_inverse)
+        if window_scaler is not None:
+            scale_x = tf.fill((total_samples, ), float(local_map_size[1] * window_scaler) * width_inverse)
+            scale_y = tf.fill((total_samples, ), float(local_map_size[0] * window_scaler) * height_inverse)
+        else:
+            # identity
+            scale_x = one
+            scale_y = one
 
         scalem = tf.stack((scale_x, zero, zero, zero, scale_y, zero, zero, zero, one), axis=1)
         scalem = tf.reshape(scalem, [total_samples, 3, 3])